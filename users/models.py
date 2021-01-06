ff27e5a8ed9e113d5efbb7d549e5364b4a0b16cf
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2021-01-06 11:31:15 +0900

    Setup + Add apps + User model & Admin
    Setup:
    - flake8
    - black

---
+++ users/models.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_JAPANESE = "jp"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_JAPANESE, "Japanese"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_AUD = "aud"
+    CURRENCY_KRW = "krw"
+    CURRENCY_JPY = "jpy"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_AUD, "AUD"),
+        (CURRENCY_KRW, "KRW"),
+        (CURRENCY_JPY, "JPY"),
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
