30caf5ac59d3b1997e8f129d86bfdf0a363a5617
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2021-01-06 16:41:31 +0900

    Add a few more Room models

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,61 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
+
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    name = models.CharField(max_length=40)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class RoomType(AbstractItem):
+
+    """ RoomType Model Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -21,4 +75,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rule = models.ManyToManyField("HouseRule", blank=True)
+
+    def __str__(self):
+        return self.name
