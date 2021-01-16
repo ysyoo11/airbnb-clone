950cbfd9b5aa3b097b09ea0980388252a2ebf21d
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2021-01-16 18:38:48 +0900

    Add pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,24 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()[:5]
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page * page_size
+    offset = limit - page_size
+    max_page = ceil(models.Room.objects.count() / page_size)
+    page_range = range(1, max_page + 1)
+    all_rooms = models.Room.objects.all()[offset:limit]
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "max_page": max_page,
+            "page_range": page_range,
+        },
+    )
