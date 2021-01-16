4b42db335dd9abb400ad36435040a2250813512f
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2021-01-16 21:19:04 +0900

    Test page() method instead of get_page()

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
+    print(page)
     page_list = models.Room.objects.all()
     paginator = Paginator(page_list, 10, orphans=4)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", context={"page": rooms})
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", context={"page": rooms})
+    except EmptyPage:
+        return redirect("/")
