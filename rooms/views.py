61a8c16a2f6b31ce56207b27acc71f05e963b38b
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2021-01-16 22:15:49 +0900

    Add pagination (CBV.ver)
    Testing class based view method

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    print(page)
-    page_list = models.Room.objects.all()
-    paginator = Paginator(page_list, 10, orphans=4)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", context={"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 4
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
