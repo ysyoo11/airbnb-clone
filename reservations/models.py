6ff2f557f073e595072f90a0c40ac519efd47ad0
Yeonsuk Yoo <yysuk0302@gmail.com>
2021-03-12 23:05:26 +0900

    Add BookedDay class (1)

--- reservations/models.py
+++ reservations/models.py
@@ -1,6 +1,17 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
+
+
+class BookedDay(core_models.TimeStampedModel):
+
+    day = models.DateField()
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Booked Day"
+        verbose_name_plural = "Booked Days"
 
 
 class Reservation(core_models.TimeStampedModel):
@@ -42,3 +53,19 @@ class Reservation(core_models.TimeStampedModel):
 
     check_completion.boolean = True
     check_completion.short_description = "is_completed"
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            start = self.check_in
+            end = self.check_out
+            difference = end - start
+            existing_booked_day = BookedDay.objects.filter(
+                reservation__room=self.room, day__range=(start, end)
+            ).exists()
+            if not existing_booked_day:
+                super().save(*args, **kwargs)
+                for i in range(difference.days + 1):
+                    day = start + datetime.timedelta(days=i)
+                    BookedDay.objects.create(day=day, reservation=self)
+                return
+        return super().save(*args, **kwargs)
