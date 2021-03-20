eaf34c4f13aa526441ac93f3acbcac12cb352a97
Yeonsuk Yoo <yysuk0302@gmail.com>
2021-03-20 15:57:50 +0900

    Update reviews/forms.py

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,6 +6,13 @@ class CreateReviewForm(forms.ModelForm):
 
     """ CreateReviewForm Definition """
 
+    cleanliness = forms.IntegerField(max_value=5, min_value=1)
+    accuracy = forms.IntegerField(max_value=5, min_value=1)
+    communication = forms.IntegerField(max_value=5, min_value=1)
+    location = forms.IntegerField(max_value=5, min_value=1)
+    check_in = forms.IntegerField(max_value=5, min_value=1)
+    value = forms.IntegerField(max_value=5, min_value=1)
+
     class Meta:
         model = models.Review
         fields = (
