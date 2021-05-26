b42f2d18eaa95c806a5c28a182864f0f656d3619
Yeonsuk Yoo <yysuk0302@gmail.com>
2021-05-27 04:10:25 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "!1mtq8u(em$19v7*+!v4svuz+cu*smd8*n=)#gv5=k#bj79tha"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 # Application definition
 
